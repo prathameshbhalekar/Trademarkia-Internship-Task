96193c45330bc674e8c7d48b82c4dc4600a5f29b
Prathamesh bhalekar <prathameshbhalekar13@gmail.com>
2022-05-20 13:45:38 +0530

    Changed Procfile Encoding

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"github.com/prathameshbhalekar/trademarkia-task/pkg/db"
 	"github.com/prathameshbhalekar/trademarkia-task/pkg/routes"
 )
 
 func main() {
-
-	env := os.Getenv("APP_ENV")
-	if env != "production" {
-		err := godotenv.Load("./../../pkg/env/.env")
-
-		if err != nil {
-			log.Fatalf("Error loading .env file")
-		}
-	}
 
 	port := os.Getenv("PORT")
 	dbUrl := os.Getenv("DB_URL")
